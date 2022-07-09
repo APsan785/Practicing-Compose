08300515ca6672dd79eb8ffa8c14b4b3dfb7594e
Adarsh <apsanprofessional@gmail.com>
2022-07-09 23:50:34 +0530

    ImageCardComposable

--- app/src/main/java/com/apsan/practicingcompose/MainActivity.kt
+++ app/src/main/java/com/apsan/practicingcompose/MainActivity.kt
@@ -1,43 +1,89 @@
 package com.apsan.practicingcompose
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.apsan.practicingcompose.ui.theme.PracticingComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PracticingComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Hey guys")
-                }
-            }
+            val painter = painterResource(id = R.drawable.ap)
+            ImageCard(
+                painter = painter,
+                contentDescription = "This is the description",
+                title = "I am AP SAN, Worship Me",
+                modifier = Modifier
+                    .fillMaxWidth(0.5F)
+                    .padding(10.dp)
+            )
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+//Box used for stacking
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(5.dp),
+        elevation = 12.dp,
+    ) {
+        Box(
+            modifier = Modifier.height(200.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, Color.Black)
+                            , startY = 320F)
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PracticingComposeTheme {
-        Greeting("Android")
+        }
+
     }
+
 }
